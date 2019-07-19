507b3ba381937607e63f1c80ee9b74554de1ef5b
Jarod Christiansen <JarodChristiansen2@gmail.com>
2019-07-18 19:26:57 -0700

    first commit rework

---
+++ Recommended Content/main.js
@@ -0,0 +1,19 @@
+document.getElementById("physical").addEventListener('click', function(){
+    document.getElementById("morp").style.display = 'none';
+    document.getElementById("myDIV").style.display = 'block';
+    document.getElementById("myDIV").style = 'margin-left: 22vw';
+})
+document.getElementById("mental").addEventListener('click', function(){
+    document.getElementById("morp").style.display = 'none';
+    document.getElementById("myDIV").style.display = 'block';
+    document.getElementById("myDIV").style = 'margin-left: 22vw';
+})
+document.getElementById("music").addEventListener('click', function(){
+    document.getElementById("myDIV").style.display = 'none';
+})
+document.getElementById("reading").addEventListener('click', function(){
+    document.getElementById("myDIV").style.display = 'none';
+})
+document.getElementById("video").addEventListener('click', function(){
+    document.getElementById("myDIV").style.display = 'none';
+})
