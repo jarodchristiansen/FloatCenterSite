ed5398b409e59a43e40f8e530aa065e1b2b94676
Jarod Christiansen <JarodChristiansen2@gmail.com>
2019-07-24 15:04:16 -0700

    update

--- First Float/main.js
+++ First Float/main.js
@@ -1,8 +1,11 @@
         onmouseover="document.getElementById('div1').style.display = 'block';"
 
-document.getElementById('physicalBenefits').addEventListener('click', function(){
- document.getElementById('muscleRecovery').style.display = 'block';
- document.getElementById('muscleRecovery').style.color = "black";       
+const muscleHeader = document.getElementById('physicalBenefits');
+const muscleRecovery = document.getElementById('muscleRecovery');
+
+muscleHeader.addEventListener('click', function(){
+ muscleRecovery.style.display = 'block';
+ muscleRecovery.style.color = "white";
 });
 
 
